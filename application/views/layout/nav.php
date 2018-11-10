6b35b130df20b8ab77373a613438dacc031b7984
Shubham <iamacceptable@yahoo.com>
2018-11-10 21:15:46 +0530

    navbar changes to fix small ui bugs

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -9,7 +9,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
+                <li class="nav-item ml-3">
                     <a class="nav-link waves-effect active waves-light dark-grey-text font-weight-bold" href="#">
                         <i class="fa fa-home red-text"></i> Home
                         <!-- <span class="sr-only">(current)</span> -->
@@ -27,7 +27,7 @@
                     <a class="nav-link dropdown-toggle waves-effect waves-light dark-grey-text font-weight-bold" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
                         <i class="fa fa-user red-text"></i> Profile </a>
-                    <div class="dropdown-menu dropdown-menu-right dropdown-blue" aria-labelledby="navbarDropdownMenuLink-4">
+                    <div class="dropdown-menu dropdown-menu-left dropdown-blue" aria-labelledby="navbarDropdownMenuLink-4">
                         <a class="dropdown-item waves-effect waves-light" href="#"><i class="fa fa-cart-plus red-text"></i> My Cart <span class="badge badge-success">0</span></a>
                         <a class="dropdown-item waves-effect waves-light" href="#"><i class="fa fa-user-o red-text"></i> My Account</a>
                         <a class="dropdown-item waves-effect waves-light" href="#"><i class="fa fa-sign-out red-text"></i> Log out</a>
