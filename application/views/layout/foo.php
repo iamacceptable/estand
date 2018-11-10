9691d9266b63f977060e7c13506bc542e657f061
Shubham <iamacceptable@yahoo.com>
2018-11-10 21:36:49 +0530

    footer seperated from the homepage to foo.php

---
+++ application/views/layout/foo.php
@@ -0,0 +1,128 @@
+<footer class="page-footer text-center text-md-left stylish-color-dark pt-0">
+
+        <div style="background-color: red;">
+            <div class="container">
+                <!--Grid row-->
+                <div class="row py-4 d-flex align-items-center">
+
+                    <!--Grid column-->
+                    <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+                        <h6 class="mb-0 font-weight-bold white-text">Get connected with us on social networks!</h6>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6 col-lg-7 text-center text-md-right">
+                        <!--Facebook-->
+                        <a class="fb-ic ml-0 px-2">
+                            <i class="fa fa-facebook white-text"> </i>
+                        </a>
+                        <!--Twitter-->
+                        <a class="tw-ic px-2">
+                            <i class="fa fa-twitter white-text"> </i>
+                        </a>
+                        <!--Google +-->
+                        <a class="gplus-ic px-2">
+                            <i class="fa fa-google-plus white-text"> </i>
+                        </a>
+                        <!--Linkedin-->
+                        <a class="li-ic px-2">
+                            <i class="fa fa-linkedin white-text"> </i>
+                        </a>
+                        <!--Instagram-->
+                        <a class="ins-ic px-2">
+                            <i class="fa fa-instagram white-text"> </i>
+                        </a>
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+            </div>
+        </div>
+
+        <!--Footer Links-->
+        <div class="container mt-5 mb-4 text-center text-md-left">
+            <div class="row mt-3">
+
+                <!--First column-->
+                <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+                    <h6 class="text-uppercase font-weight-bold">
+                        <strong>Elpedia Welfare Society</strong>
+                    </h6>
+                    <hr class="red mb-4 mt-0 d-inline-block mx-auto" style="width: 80px;">
+                    <p>Elpedia welfare technologies is company supported by a group of high end professionals. In the team there are professors from IIT Delhi, Engineers, Doctors, Students and last but the most important, a group of users. User is the most important member who help to customize product as per their actual need. It is this combination of people which gives this group a unique identity.</p>
+                </div>
+                <!--/.First column-->
+
+                <!--Second column-->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 class="text-uppercase font-weight-bold">
+                        <strong>Products</strong>
+                    </h6>
+                    <hr class="red mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <p>
+                        <a href="#!">E-Stand Laptop Stand</a>
+                    </p>
+                    <p>
+                        <a href="#!">E-Stand Desktop Stand</a>
+                    </p>
+                    <p>
+                        <a href="#!">E-Stand Slope</a>
+                    </p>
+                    <p>
+                        <a href="#!">Visit More...</a>
+                    </p>
+                </div>
+                <!--/.Second column-->
+
+                <!--Third column-->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 class="text-uppercase font-weight-bold">
+                        <strong>Useful links</strong>
+                    </h6>
+                    <hr class="red mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <p>
+                        <a href="#!">My Account</a>
+                    </p>
+                    <p>
+                        <a href="#!">Our Blog</a>
+                    </p>
+                    <p>
+                        <a href="#!">Payment & Return</a>
+                    </p>
+                    <p>
+                        <a href="#!">Help</a>
+                    </p>
+                </div>
+                <!--/.Third column-->
+
+                <!--Fourth column-->
+                <div class="col-md-4 col-lg-3 col-xl-3">
+                    <h6 class="text-uppercase font-weight-bold">
+                        <strong>Contact</strong>
+                    </h6>
+                    <hr class="red mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <p>
+                        <i class="fa fa-home mr-3"></i> Fm-1, Plot no.67, Rajendra Nagar, Sector-5, Sahibabad, Ghaziabad 201005</p>
+                    <p>
+                        <i class="fa fa-envelope mr-3"></i> contact@elpediainfotech.com</p>
+                    <p>
+                        <i class="fa fa-phone mr-3"></i> + 91 991 177 4882</p>
+                </div>
+                <!--/.Fourth column-->
+
+            </div>
+        </div>
+        <!--/.Footer Links-->
+
+        <!-- Copyright-->
+        <div class="footer-copyright py-3 text-center">
+            <div class="container-fluid">
+                All Rights Reserved | eStand.in © 2018 | Developed With <i class="red-text fa fa-heart"></i> By: <a href="https://mdbootstrap.com/bootstrap-tutorial/" target="_blank"> Shubham Kumar </a>
+            </div>
+        </div>
+        <!--/.Copyright -->
+
+    </footer>
