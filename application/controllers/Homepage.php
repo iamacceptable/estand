e5af0341d7164d36fe1cd36ec0af361d2dfbc06a
Shubham <iamacceptable@yahoo.com>
2018-11-15 20:14:45 +0530

    products section start

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -3,8 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Homepage extends CI_Controller {
 	public function index()
-	{
-		$this->load->view('Homepage/homepage');
+	{	
+		$data['active'] = 'homepage';
+		$this->load->view('Homepage/homepage',$data);
 	}
 	
 }
---
+++ application/views/Products/products.php
@@ -0,0 +1,7 @@
+<?php $this->view('layout/header');?>
+<body>
+    <header>
+        <?php $this->view('layout/nav');?>
+    </header>
+    <?php $this->view('layout/foo');?>
+<?php $this->view('layout/footer'); ?>
