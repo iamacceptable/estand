29b36a10df5205b34fb997ec26905d84d58619b9
Shubham <iamacceptable@yahoo.com>
2018-12-05 22:40:17 +0530

    admin homepage caurosel ready

--- application/views/Admin/admin_homepage.php
+++ application/views/Admin/admin_homepage.php
@@ -7,7 +7,7 @@
 
     <!-- Nav tabs -->
     <div class="row">
-      <div class="col-md-3 col-lg-3 col-sm-3">
+      <div class="col-md-3 col-lg-3 col-sm-3 " style="padding: 0; ">
         <ul class="nav md-pills pills-danger flex-column" role="tablist">
           <li class="nav-item">
             <a class="nav-link active" data-toggle="tab" href="#homepage" role="tab">
@@ -20,18 +20,66 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#panel23" role="tab">Billing details
-              <i class="fa fa-address-card ml-2"></i>
+            <a class="nav-link" data-toggle="tab" href="#panel23" role="tab">
+              <i class="fa fa-address-card mr-2"></i>Products and Categories
             </a>
           </li>
         </ul>
       </div>
-      <div class="col-md-9 col-sm-9 col-lg-9 ">
+      <div class="col-md-9 col-sm-9 col-lg-9">
         <!-- Tab panels -->
         <div class="tab-content vertical">
           <!-- Panel 1 -->
-          <div class="tab-pane fade in show active" id="homepage" role="tabpanel">
-            <h5 class="my-2 h5">Panel 1</h5>
+          <div class="tab-pane fade in show active my-2" id="homepage" role="tabpanel">
+            <div class="container ">
+            	<div class="row">
+            		<div class="col mt-4">
+            			<h4 class="font-weight-bold red-text"><i class="fa fa-home mx-2"></i>Homepage Content</h4>
+            		</div>
+            	</div>
+            	<hr>
+            	<div class="row">
+            		<div class="col-md-2 col-sm-0 col-xs-0 col-lg-2"></div>
+            		<div class="col-md-8 col-sm-8 col-xs-8 col-lg-8">
+            			<div class="card">
+							<div class="card-body">
+								<h5 class="card-title text-center">Homepage Carousel</h5>
+								<h6 class="card-subtitle mb-2 text-muted text-center">Add new Image to Carousel</h6>
+								<form action="#" method="POST" class="md-form">
+									<div class="md-form">
+								        <input type="text" id="title" name="cotitle" class="form-control">
+								        <label for="title">Carousel Title</label>
+								    </div>
+								    <div class="md-form">
+								        <input type="text" id="description" length="100" name="codescription" class="form-control">
+								        <label for="description">Carousel Description</label>
+								    </div>
+								    <div class="file-field">
+								        <div class="btn btn-danger btn-sm float-left">
+								            <span>Choose file</span>
+								            <input type="file">
+								        </div>
+								        <div class="file-path-wrapper">
+								            <input class="file-path" readonly type="text" placeholder="Upload your file">
+								        </div>
+								    </div>
+								    <div class="text-center md-form">
+								    	<button class="btn btn-success btn-sm">Add New<i class="fa fa-plus ml-2"></i></button>
+									</div>	
+								</form>
+							</div>
+						</div>
+            		</div>
+            		<div class="col-md-2 col-sm-0 col-xs-0 col-lg-2"></div>
+            	</div>
+            	<hr>
+            	<div class="row">
+            		<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+            			<h4 class="font-weight-bold indigo-text text-center">Current Slide Show Images</h4>
+            		</div>
+            		
+            	</div>
+            </div>
           </div>
           <!-- Panel 1 -->
           <!-- Panel 2 -->
--- application/views/Admin/layout/nav.php
+++ application/views/Admin/layout/nav.php
@@ -4,11 +4,11 @@
         <a class="navbar-brand font-weight-bold" href="#">
             <strong class="white-text"><i class="fa fa-user-secret mr-2"></i>Admin Panel</strong>
         </a>
-        <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4"
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4"
             aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon white-text"></span>
-        </button> -->
-        <div class=" navbar-collapse" id="navbarSupportedContent-4">
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item ml-3">
                     <a class="nav-link waves-effect active waves-light orange-text font-weight-bold" href="<?php echo base_url();?>index.php/Navbar/home">
